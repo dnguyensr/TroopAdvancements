0b67d115f65dfd659ac20c74134ed6066c1a3b10
dnguyensr <danielnguyensr@gmail.com>
2016-11-16 17:46:57 -0800

    Edit user associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   has_one :advancements
-  has_many :ranks, through :advancements
+  has_many :ranks, through: :advancements
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
