f099128b6568b11af404b613a8fd915623fef1d8
dnguyensr <danielnguyensr@gmail.com>
2016-11-16 17:51:39 -0800

    Update seed with each user type

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Add Users
+adult_leader = User.create(first_name: "Bill", last_name: "Nye", adult_leader: true, parent: false, email: 'a@a.com', password: 'password')
+parent = User.create(first_name: "Bob", last_name: "Hope", adult_leader: false, parent: true, email: 'b@b.com', password: 'password')
+scout = User.create(first_name: "Adam", last_name: "Sandler", adult_leader: false, parent: false, email: 'boy@scout.com', password: 'password')
