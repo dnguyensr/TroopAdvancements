ed8323a67b679c062ba56fb6950da65fd8798786
dnguyensr <danielnguyensr@gmail.com>
2016-11-18 20:42:59 -0800

    Fix associations, add ranks to seed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  has_one :advancements
-  has_many :ranks, through: :advancements
+  has_one :advancement
+  has_many :ranks, through: :advancement
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
